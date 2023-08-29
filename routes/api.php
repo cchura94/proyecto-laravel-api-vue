f0c8e673876ec17322f21f49d5ef575a58064fb8
cristian <cchura.cpc@gmail.com>
2023-08-28 21:16:21 -0400

    registro api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Autenticación Api 
+Route::prefix("v1/auth")->group(function(){
+
+    //  POST /api/v1/auth/login  -    AuthController@funIngresar
+    Route::post("/login", [AuthController::class, "funIngresar"]);
+    //  POST /api/v1/auth/register  -    AuthController@funRegistro
+    Route::post("/register", [AuthController::class, "funRegistro"]);
+    
+    Route::middleware('auth:sanctum')->group(function(){
+
+        //  GET  /api/v1/auth/profile  -    AuthController@funPerfil
+        Route::get("/profile", [AuthController::class, "funPerfil"]);
+        //  POST /api/v1/auth/logout  -    AuthController@funSalir
+        Route::post("/logout", [AuthController::class, "funSalir"]);
+
+    });
 });
