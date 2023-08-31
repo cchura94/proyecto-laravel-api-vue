21e547c7d7a4b9f51f9eff59e695c88318b07807
cristian <cchura.cpc@gmail.com>
2023-08-31 15:56:21 -0400

    api auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,15 +4,36 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function funIngresar(Request $request){
+        // validar
+        $credenciales = $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+        // verificar y autenticar
+        if(!Auth::attempt($credenciales)){
+            return response()->json(["message" => "No Autenticado"], 401);
+        }
+
+        // generamos el token
+        // $usuario = Auth::user();
+        // $token = $usuario->createToken("token personal")->plainTextToken;
+        $token = $request->user()->createToken("token personal")->plainTextToken;
+
+        return response()->json([
+            "access_token" => $token,
+            "token_type" => "Bearer",
+            "usuario" => $request->user()
+        ]);
 
     }
 
     public function funRegistro(Request $request){
-        // validar (Accept: application/json)
+        // validar (Accept: application/json) 302
         $request->validate([
             "name" => "required",
             "email" => "required|email|unique:users",
@@ -28,11 +49,18 @@ class AuthController extends Controller
         return response()->json(["message" => "Usuario registrado", "data"=> $user], 201);
     }
 
-    public function funPerfil(){
-
+    public function funPerfil(Request $request){
+        // $usuario = $request->user()->persona;
+        // $usuario = Auth::user()->with("persona");
+        // $usuario = User::with('persona')->find(Auth::user()->id);
+        $usuario = User::with('persona')->find(Auth::id());
+        return response()->json($usuario, 200);
     }  
 
-    public function funSalir(){
+    public function funSalir(Request $request){
+        $usuario = $request->user();
+        $usuario->tokens()->delete();
 
+        return response()->json(["message" => "Log OUT"]);
     }
 }
